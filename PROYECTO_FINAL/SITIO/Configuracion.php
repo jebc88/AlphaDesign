f094417ba21e03ccae5779ba4b949ecb02778787
Esteban Benavides <jebc88@hotmail.com>
2015-03-28 11:01:49 -0600

    Estructura de sitio y carpeta VM 28 de marzo

---
+++ PROYECTO_FINAL/SITIO/Configuracion.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Configuracion</title>
+</head>
+<body>
+
+
+</body>
+</html>
---
+++ PROYECTO_FINAL/SITIO/Respaldos.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Esteban
+ * Date: 28/03/2015
+ * Time: 10:27 AM
+ */ 
