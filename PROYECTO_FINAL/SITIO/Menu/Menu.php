0306971acf879ffcb6d48fad683e2440819ca3fa
Esteban Benavides <jebc88@hotmail.com>
2015-03-28 14:32:12 -0600

    Creacion de archivos empty en carpetas vacias

---
+++ PROYECTO_FINAL/SITIO/Menu/Menu.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Menu</title>
+</head>
+<body>
+    <div>
+        <h1>1 Nivel</h1>
+        <ul><a href="../Login.php">Login</a></ul>
+    </div>
+
+    <div>
+        <h1>2 Nivel</h1>
+        <ul><a href="../Index.php">Index</a></ul>
+    </div>
+
+    <div>
+        <h1>3 Nivel</h1>
+        <ul><a href="../Help.html">Help</a></ul>
+        <ul><a href="../MaquinaVirtual.php">Maquina Virtual</a></ul>
+        <ul><a href="../Administracion.php">Administracion</a></ul>
+    </div>
+
+    <div>
+        <h1>4 Nivel</h1>
+        <ul><a href="../Configuracion.php">Configuracion</a></ul>
+        <ul><a href="../Respaldos.php">Respaldos</a></ul>
+    </div>
+</body>
+</html>
